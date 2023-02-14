ede99787c6471d4a7bbb02edac4fef861b501e1f
Jax DesMarais-Leder <jdesmarais@paypal.com>
2023-02-14 08:58:23 -0600

    just trying stuff

--- Sources/BraintreeApplePay/Public/BraintreeApplePay/BraintreeApplePay.h
+++ Sources/BraintreeApplePay/Public/BraintreeApplePay/BraintreeApplePay.h
@@ -12,7 +12,6 @@ FOUNDATION_EXPORT const unsigned char BraintreeApplePayVersionString[];
 #import <Braintree/BTApplePayClient.h>
 #import <Braintree/BTApplePayCardNonce.h>
 #elif SWIFT_PACKAGE
-#import <BraintreeCore/BraintreeCore-Swift.h>
 #import <BraintreeApplePay/BTApplePayClient.h>
 #import <BraintreeApplePay/BTApplePayCardNonce.h>
 #else
