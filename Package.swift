d7b6f7449144e02a95b92bf282bf170a74cc4b92
Jax DesMarais-Leder <jdesmarais@paypal.com>
2023-02-15 09:50:32 -0600

    try something else with header paths

--- Package.swift
+++ Package.swift
@@ -62,9 +62,9 @@ let package = Package(
         .target(
             name: "BraintreeApplePay",
             dependencies: ["BraintreeCore"],
-            publicHeadersPath: "./",
+            publicHeadersPath: "..",
             cSettings: [
-                .headerSearchPath("./")
+                .headerSearchPath("..")
             ]
         ),
         .target(
